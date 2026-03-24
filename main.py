290e45af9deba63a8a4612154c52c9a8cfdd2337
bsreng <bsreng007@gmail.com>
2026-03-24 08:09:26 -0400

    updated UI

--- main.py
+++ main.py
@@ -1,1 +1,79 @@
-#TBD
+import socketio
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+
+# I'll be honest, no idea if this next import is necessary, but it seems useful
+from fastapi.middleware.cors import CORSMiddleware
+
+fastAPIApp = FastAPI()
+fastAPIApp.add_middleware(CORSMiddleware, allow_origins=["*"])
+
+#serves static files (JS, CSS, HTML) from the /static folder
+fastAPIApp.mount("/static", StaticFiles(directory="static"), name="static")
+
+# Dict which stores Socket ID as key and [room, username] as value
+SIDDataTracker = {}
+# Dict which stores existing usernames in a room
+roomUserTracker = {}
+
+sessionID = socketio.AsyncServer(async_mode = 'asgi', cors_allowed_origins='*')
+
+app = socketio.ASGIApp(sessionID, fastAPIApp)
+
+# TODO: Still needs to serve a webpage. When you have it done, put it here.
+
+@sessionID.event
+async def connect(sid, environ):
+    """Normal connect, but also adds info to tracker array."""
+    global SIDDataTracker
+    SIDDataTracker[sid] = ["unset", ""]
+
+@sessionID.event
+async def disconnect(sid):
+    """Normal disconnect, but also removes info from tracker arrays."""
+    global SIDDataTracker, roomUserTracker
+    room = SIDDataTracker[sid][0]
+    user = SIDDataTracker[sid][1]
+    if not (SIDDataTracker[sid][0] == "unset"):
+        await sessionID.emit("message", f"{user} has left the room", to=room)
+        roomUserTracker[room].remove(user)
+        if not roomUserTracker[room]:
+            del roomUserTracker[room]
+    del SIDDataTracker[sid]
+
+
+@sessionID.event
+async def message(sid, data):
+    """Sends message to all in client's room. Message should be recieved from here - no polling necessary."""
+    global SIDDataTracker
+    if (SIDDataTracker[sid][0] == "unset"):
+        raise Exception("User is not in a room")
+    await sessionID.emit("message", data["text"], to=SIDDataTracker[sid][0])
+
+@sessionID.event
+async def updateInfo(sid, data):
+    """Updates usernames and room IDs on the tracker. Also adds and removes from rooms based on arguments."""
+    global SIDDataTracker, roomUserTracker
+    if not (SIDDataTracker[sid][0] == "unset"):
+        await sessionID.leave_room(sid, SIDDataTracker[sid][0])
+        roomUserTracker[SIDDataTracker[sid][0]].remove(SIDDataTracker[sid][1])
+        if not roomUserTracker[SIDDataTracker[sid][0]]:
+            del roomUserTracker[SIDDataTracker[sid][0]]
+
+    SIDDataTracker.update({sid: [data["room"], data["user"]]})
+
+    if not (data["room"] == "unset"):
+        if (data["room"] in roomUserTracker):
+            roomUserTracker[data["room"]].append(data["user"])
+        else:
+            roomUserTracker[data["room"]] = [data["user"]]
+
+        await sessionID.enter_room(sid, data["room"])
+
+@sessionID.event
+async def validate(sid, data):
+    if data["user"] in roomUserTracker.get(data["room"], []):
+        return await validate(sid, {"room": data["room"], "user": data["user"] + "(+) "})
+    else:
+        return data["user"]
